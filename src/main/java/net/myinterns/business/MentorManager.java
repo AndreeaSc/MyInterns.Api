b98b7fcdcb1e1519dff76baa2b7f1d721e09e31a
Andreea Schiau <schiau.andreea@gmail.com>
2019-12-19 14:59:03 +0200

    add commons project

---
+++ src/main/java/net/myinterns/business/MentorManager.java
@@ -0,0 +1,21 @@
+package net.myinterns.business;
+
+import java.util.List;
+
+import net.andreea.MyInterns.persistance.entity.User;
+
+public interface MentorManager {
+	
+	List<MentorManager> getAll();
+
+	MentorManager getById(int id);
+
+	void saveOrUpdate(final String name, final String surname, final String email, final String qualification,
+			final Boolean isExternal, final User user);
+
+	void saveOrUpdate(final MentorManager mentor);
+
+	void delete(long id);
+
+//	List<Mentor> getAllMentors();
+}
