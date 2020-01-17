abdf499914acc512fb28f4dc63daef33f488f9a8
Andreea Schiau <schiau.andreea@gmail.com>
2020-01-17 11:58:58 +0200

    add user and student in one method

--- src/main/java/net/myinterns/business/impl/StudentManagerImpl.java
+++ src/main/java/net/myinterns/business/impl/StudentManagerImpl.java
@@ -1,84 +1,55 @@
-package net.myinterns.business.impl;
-
+	package net.myinterns.business.impl;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
 import net.andree.MyInterns.common.dto.StudentDTO;
 import net.andree.MyInterns.common.dto.UserDTO;
 import net.myinterns.business.StudentManager;
 import net.myinterns.persistance.dao.StudentDao;
 import net.myinterns.persistance.entity.Student;
 import net.myinterns.persistance.entity.User;
-
 @Repository
 public class StudentManagerImpl implements StudentManager {
-
 	@Autowired
 	StudentDao studentDao;
-
 	public void setStudentDao(StudentDao studentDao) {
 		this.studentDao = studentDao;
 	}
-
 	@Override
 	public List<StudentDTO> getAll() {
-
 		return studentDao.getAll();
 	}
-
 	@Override
 	public StudentDTO getById(long id) {
-
 		Student student = new Student();
 		student = studentDao.getById(id);
-
 		StudentDTO studentDTO = new StudentDTO();
-
 		studentDTO.setName(student.getName());
 		studentDTO.setSurname(student.getSurname());
 		studentDTO.setDescription(student.getDescription());
 		studentDTO.setEmail(student.getEmail());
-
 		return studentDTO;
 	}
-
 	@Override
 	public StudentDTO saveOrUpdate(String name, String surname, String description, String email, UserDTO userDTO) {
-
 		studentDao.saveOrUpdate(name, surname, description, email, userDTO);
-
 		StudentDTO studentDTO = new StudentDTO();
 		studentDTO.setName(name);
 		studentDTO.setSurname(surname);
 		studentDTO.setDescription(description);
 		studentDTO.setEmail(email);
-
 		return studentDTO;
 	}
-
 	@Override
 	public StudentDTO saveOrUpdate(StudentDTO studentDTO) {
-
-//		String name = studentDTO.getName();
-//		String surname = studentDTO.getSurname();
-//		String description = studentDTO.getDescription();
-//		String email = studentDTO.getDescription();
-//		
-//		studentDao.saveOrUpdate(name, surname, description, email);
-
+		
 		return null;
 	}
-
 	@Override
 	public void delete(long id) {
-
 		studentDao.delete(id);
 	}
-
 	public StudentDTO updateByEmail(StudentDTO studentDTO, String emailStatic) {
-
 		Student student = new Student();
 		student.setName(studentDTO.getName());
 		student.setSurname(studentDTO.getSurname());
@@ -86,14 +57,11 @@ public class StudentManagerImpl implements StudentManager {
 		student.setEmail(studentDTO.getEmail());
 		
 		Student studentUpdate = new Student();
-
 		studentUpdate = studentDao.updateByEmail(student, emailStatic);
-
 		studentDTO.setName(student.getName());
 		studentDTO.setSurname(student.getSurname());
 		studentDTO.setDescription(student.getDescription());
 		studentDTO.setEmail(student.getEmail());
-
 		return studentDTO;
 	}
 }
--- src/main/java/net/myinterns/common/controller/StudentController.java
+++ src/main/java/net/myinterns/common/controller/StudentController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -19,11 +18,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import net.andree.MyInterns.common.dto.StudentDTO;
-import net.andree.MyInterns.common.dto.UserDTO;
-import net.andree.MyInterns.common.dto.StudentDTO;
 import net.myinterns.business.StudentManager;
 import net.myinterns.persistance.dao.StudentDao;
 import net.myinterns.persistance.entity.Student;
+import net.myinterns.persistance.entity.User;
 
 @Path("/student")
 public class StudentController {
@@ -72,7 +70,7 @@ public class StudentController {
 			studentS.setEmail(email1);
 
 			System.out.println("Student updated as: " + studentS.toString());
-			
+
 			return studentDao.updateByEmail(studentS, email);
 
 		} catch (Exception e) {
@@ -85,6 +83,41 @@ public class StudentController {
 	}
 
 	@POST
+	@Path("/addDTO")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Response addDTO(String studentUser) throws JSONException {
+		System.out.println("Add user student dto controller");
+		JSONObject jsonObj;
+
+		try {
+			jsonObj = new JSONObject(studentUser);
+			String name = jsonObj.getString("name");
+			String surname = jsonObj.getString("surname");
+			String description = jsonObj.getString("description");
+			String email = jsonObj.getString("email");
+			String username = jsonObj.getString("username");
+			String password = jsonObj.getString("password");
+
+			User user = new User();
+			user.setUsername(username);
+			user.setPassword(password);
+
+			Student student = new Student();
+			student.setName(name);
+			student.setSurname(surname);
+			student.setDescription(description);
+			student.setEmail(email);
+			student.setUser(user);
+
+			studentDao.saveOrUpdateDAO(student);
+
+			return Response.ok(student).build();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	@POST
 	@Path("/add")
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	public Response add(Student student) throws JSONException {
@@ -92,12 +125,12 @@ public class StudentController {
 		studentDao.saveOrUpdateDAO(student);
 		return Response.ok(student).build();
 
-//		JSONObject jsonObj, jsonObjUsername;
-//
-//		jsonObj = new JSONObject(student);
-//		jsonObjUsername = jsonObj.getJSONObject("user");
-////		String jsonObjUsername = jsonObj.getJSONObject("user").getString("username");
-////		String jsonObjPassword = jsonObj.getJSONObject("user").getString("password");
+//		JSONObject jsonObj;
+
+//		JSONOBJ = NEW JSONOBJECT(STUDENT);
+//		jsonObj = jsonObj.getJSONObject("user");
+//		String jsonObjUsername = jsonObj.getString("user");
+//		String jsonObjPassword = jsonObj.getString("password");
 //		UserDTO userDTO = new UserDTO();
 //		String jsonUsername = jsonObjUsername.getString("username");
 ////		userDTO.setUsername(jsonObjUsername);
